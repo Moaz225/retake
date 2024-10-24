46f262e390123184e54fef1db92cec2dcc529244
Moaz225 <130934796+Moaz225@users.noreply.github.com>
2024-10-24 12:51:20 +0500

    Create AuthorService.java

---
+++ 3.7/AuthorService.java
@@ -0,0 +1,15 @@
+// src/main/java/<your-package>/service/AuthorService.java
+@Service
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+}
